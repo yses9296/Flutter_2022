3214c70b753783bcd77cff4d5c30669801c2e6d5
Eunseo Choi <yses9296@gmail.com>
2022-08-03 23:09:48 +0900

    2022-08-03

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -12,11 +12,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.star), //left menu (like logo)
-          title: Text('This is Android Application'),
-          actions: [Icon(Icons.star), Icon(Icons.star), Icon(Icons.star), Icon(Icons.star)],//right menu
+          title: Text('당근마켓'),
+          actions: [Icon(Icons.search), Icon(Icons.menu), Icon(Icons.alarm)]
         ),
-        body: SizedBox(
+        body: Container(
+          height: 150,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Image.asset('../assets/camera.jpg', width: 200,),
+              Container(
+                width: 300,
+                // padding: EdgeInsets.fromLTRB(10, 20, 0, 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)', style: TextStyle(fontWeight: FontWeight.w900)),
+                    Text('노원구 상계 1동 10분 전'),
+                    Text('350,000원'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite),
+                        Text('4')
+                      ],
+                    ),
+                  ],
+                )
+              )
+            ],
+          )
         )
       )
     );
